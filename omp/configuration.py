d0fa0da180805ea24976e9281cd9db46594a5c6c
Mattia Barbon <mattia@barbon.org>
2017-03-03 15:31:32 +0100

    Fixes for running with Python 2

--- omp/configuration.py
+++ omp/configuration.py
@@ -1,4 +1,9 @@
-import configparser
+import sys
+
+if sys.version_info.major >= 3:
+    import configparser
+else:
+    import ConfigParser as configparser
 
 class Configuration(object):
     def __init__(self, path=None):
@@ -16,19 +21,19 @@ class Configuration(object):
     def read(self, path):
         conf = configparser.ConfigParser()
         conf.read(path)
-        conf_jdk = conf['jdk']
         self.jdk = (
-            conf_jdk['jdk_version'],
-            int(conf_jdk['jdk_update']),
-            int(conf_jdk['jdk_build']),
+            conf.get('jdk', 'jdk_version'),
+            conf.getint('jdk', 'jdk_update'),
+            conf.getint('jdk', 'jdk_build'),
         )
-        self.bits = int(conf_jdk['bits'])
+        self.bits = conf.getint('jdk', 'bits')
 
     def write(self, path):
         conf = configparser.ConfigParser()
-        conf['jdk'] = {'jdk_version': self.jdk[0],
-                       'jdk_update': self.jdk[1],
-                       'jdk_build': self.jdk[2],
-                       'bits': self.bits}
+        conf.add_section('jdk')
+        conf.set('jdk', 'jdk_version', self.jdk[0])
+        conf.set('jdk', 'jdk_update', str(self.jdk[1]))
+        conf.set('jdk', 'jdk_build', str(self.jdk[2]))
+        conf.set('jdk', 'bits', str(self.bits))
         with open(path, 'w') as out:
             conf.write(out)
