98433f9fee7d2460516d37272189dd676d475f27
Mattia Barbon <mattia@barbon.org>
2017-03-03 18:19:08 +0100

    Quick & dirty tweaks to build/test a 32-bit build

--- build_all.py
+++ build_all.py
@@ -9,6 +9,12 @@ import omp
 def _configure_openjdk(jdkdir, cc, cxx, bits=64):
     subprocess.check_call(['sh', './configure', '--with-target-bits=%s' % bits, 'CC=%s' % cc, 'CXX=%s' % cxx], cwd=jdkdir)
 
+def _bits_flags(ojdk):
+    if ojdk.conf.bits == 64:
+        return '-m64'
+    else:
+        return '-m32'
+
 def build_openjdk(conf):
     jdkdir = conf.jdk_directory()
     _configure_openjdk(jdkdir, 'gcc-5', 'g++-5', conf.bits)
@@ -17,14 +23,18 @@ def build_openjdk(conf):
 
 def build_honest_profiler(ojdk):
     ojdk.find_jdk_build_directory()
-
-    subprocess.check_call(['cmake', '.'], cwd='honest-profiler', env=ojdk.with_java_home())
+    bits_flags = _bits_flags(ojdk)
+    bits_options = [opt % bits_flags for opt in (
+        '-DCMAKE_C_FLAGS=%s',
+        '-DCMAKE_CXX_FLAGS=%s',
+        '-DCMAKE_SHARED_LINKER_FLAGS=%s')]
+    subprocess.check_call(['cmake', '.'] + bits_options, cwd='honest-profiler', env=ojdk.with_java_home())
     subprocess.check_call(['mvn', 'package', '-DskipTests=true'], cwd='honest-profiler')
 
 def build_tests(ojdk):
     ojdk.find_jdk_build_directory()
-
-    subprocess.check_call(['mvn', 'package', '-DskipTests=true'], cwd='test', env=ojdk.with_java_home())
+    bits_flags = _bits_flags(ojdk)
+    subprocess.check_call(['mvn', 'package', '-DskipTests=true', '-Djdk.bits.flags=%s' % bits_flags], cwd='test', env=ojdk.with_java_home())
 
 if __name__ == '__main__':
     conf = omp.configuration.Configuration('configuration.ini')
